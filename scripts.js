ec2cf9549fa58061395a40aac68b21c2d9db03ea
defowler2005 <129338316+defowler2005@users.noreply.github.com>
2023-10-04 15:37:40 +1030

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,14 @@
 function main() {
+    var elements = document.getElementsByClassName('themeable');
+    var savedTheme = localStorage.getItem('theme') || 'light';
+    for (var i = 0; i < elements.length; i++) elements[i].classList.toggle('dark-theme', savedTheme === 'dark');
     eruda.init();
     hljs.highlightAll();
+};
+
+function toggleTheme() {
+    var elements = document.getElementsByClassName('themeable');
+    for (var i = 0; i < elements.length; i++) elements[i].classList.toggle('dark-theme');
+    var isDarkTheme = document.body.classList.contains('dark-theme');
+    localStorage.setItem('theme', isDarkTheme ? 'dark' : 'light');
 };
